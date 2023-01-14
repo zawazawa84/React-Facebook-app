0df43256f142fc5cb920e8ccbd53775c6dd35e99
aizawa <ikiatawazia@outlook.jp>
2023-01-14 18:17:31 +0900

    Rightbar　HTML CSS　スタイリング

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -3,6 +3,33 @@ import "./Rightbar.css"
 
 export default function Rightbar() {
   return (
-    <div className='rightbar'>Rightbar</div>
+    <div className='rightbar'>
+      <div className="rightWrapper">
+        <div className="eventContainer">
+            <img src="assets/star.png" alt="" className="starImg" />
+            <span className="eventText">
+              <b>フォロワー限定</b>イベント開催中！
+            </span>
+        </div>
+        <img src="assets/ad.jpeg" alt="" className='eventImg'/>
+        <h4 className="rightbarTitle">オンラインの友達</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+                <img src="assets/person/1.jpeg" alt="" className='rightbarProfileImg'/>
+                <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Shin Code</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+                <img src="assets/person/2.jpeg" alt=""  className='rightbarProfileImg'/>
+                <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Tanaka</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
