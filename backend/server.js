d4ffb63ce88f753cf144daf41d128dd102d19faf
aizawa <ikiatawazia@outlook.jp>
2023-01-21 02:56:02 +0900

    MongoDBとNode.jsの接続

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const app = express();
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
 const PORT = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+    .connect(process.env.MONGOURL)
+    .then(() => {
+        console.log("DBと接続中・・・")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.get("/", (req, res)=>{
-    res.send("hello ");
+    res.send("hello world");
 })
 
 app.listen(PORT, () => console.log("サーバーが起動しました"))
