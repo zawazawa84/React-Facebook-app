19af8731e6436230e479eb7752ea7fcd1abe4047
aizawa <ikiatawazia@outlook.jp>
2023-01-21 16:46:30 +0900

    ユーザー情報を更新するAPI

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,24 @@
 const router = require("express").Router()
+const User = require("../models/User")
+
+// ユーザー情報の更新
+router.put("/:id", async (req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json("ユーザー情報が更新されました")
+        } catch (err) {
+            return res.status(500).json(err)
+        }
+    } else {
+        return res.status(403).json("あなたは自分のアカウントの時だけ情報を更新できます")
+    }
+})
+// ユーザー情報の削除
+// ユーザー情報の取得
+
 
 
 module.exports = router;
