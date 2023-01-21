ea8f8320b4e6d46777aa10a773cb8b7ffe89106b
aizawa <ikiatawazia@outlook.jp>
2023-01-22 00:33:13 +0900

    投稿に「いいね」を押せるAPI

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -51,5 +51,28 @@ router.get("/:id", async (req, res) => {
     }
 })
 
+router.put("/:id/like", async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if (!post.likes.includes(req.body.userId)) {
+            await post.updateOne({
+                $push: {
+                    likes: req.body.userId,
+                }
+            });
+            return res.status(200).json("投稿にいいねを押しました")
+        } else {
+            await post.updateOne({
+                $pull: {
+                    likes: req.body.userId
+                }
+            })
+            return res.status(200).json("投稿からいいねを外しました")
+        }
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
+
 
 module.exports = router;
