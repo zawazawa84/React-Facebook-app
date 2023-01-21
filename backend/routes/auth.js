56144e85d623357e9b2cb523bda1fc52187169ce
aizawa <ikiatawazia@outlook.jp>
2023-01-21 16:14:05 +0900

    ユーザーログインができるAPI作成

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 const User = require("../models/User")
 
+// ユーザー登録
 router.post("/register", async (req, res)=>{
     try{
         const newUser = await new User({
@@ -13,6 +14,21 @@ router.post("/register", async (req, res)=>{
         return res.status(200).json(user);
     } catch (err) {
         return res.status(500).json(err)
+    }
+})
+
+// ログイン
+router.post("/login", async (req, res) => {
+    try{
+        const user = await User.findOne({email: req.body.email});
+        if (!user) return res.status(404).send("ユーザーが見つかりません");
+
+        const vailedPassword = req.body.password === user.password;
+        if (!vailedPassword) return res.status(400).json("パスワードが違います")
+
+        return res.status(200).json(user);
+    }catch (err) {
+        return res.status(200).json(err)
     }
 })
 
