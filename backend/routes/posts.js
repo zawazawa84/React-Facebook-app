6c693e038cd09060f01227921600148c7a4caf3b
aizawa <ikiatawazia@outlook.jp>
2023-01-22 00:15:26 +0900

    投稿を取得するAPI

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -41,4 +41,15 @@ router.delete("/:id", async (req, res) => {
     }
 })
 
+router.get("/:id", async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        return res.status(200).json(post)
+        
+    } catch (err) {
+        return res.status(403).json(err);
+    }
+})
+
+
 module.exports = router;
