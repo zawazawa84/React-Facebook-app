e92de96ab4b71c986b72d6ad9b396f9d28ec73e8
aizawa <ikiatawazia@outlook.jp>
2023-01-21 18:16:50 +0900

    ユーザーをフォローするAPI

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -41,4 +41,33 @@ router.get("/:id", async (req, res) => {
     }
 })
 
+// ユーザーのフォロー
+router.put("/:id/follow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try{
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (!user.followers.includes(req.body.userId)) {
+                await user.updateOne({
+                    $push: {
+                        follower: req.body.userId,
+                    }
+                });
+                await currentUser.updateOne({
+                    $push: {
+                        followings: req.params.id
+                    }
+                })
+                return res.status(200).json("フォローに成功しました")
+            } else {
+                return res.status(403).json("あなたはすでにこのユーザーをフォローしています")
+            }
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(500).json("自分自身をフォローできません");
+    }
+})
+
 module.exports = router;
