35c156f95833e0bee88e5efd69be611f520ac55a
aizawa <ikiatawazia@outlook.jp>
2023-01-21 17:31:12 +0900

    ユーザー情報を取得するAPI

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -21,17 +21,24 @@ router.delete("/:id", async (req, res) => {
     if(req.body.userId === req.params.id || req.body.isAdmin){
         try{
             const user = await User.findByIdAndDelete(req.params.id);
-            res.status(200).json("ユーザー情報が削除されました")
+            res.status(200).json("ユーザー情報が削除されました");
         } catch (err) {
-            return res.status(500).json(err)
+            return res.status(500).json(err);
         }
     } else {
-        return res.status(403).json("あなたは自分のアカウントの時だけ削除できます")
+        return res.status(403).json("あなたは自分のアカウントの時だけ削除できます");
     }
 })
 
 // ユーザー情報の取得
-
-
+router.get("/:id", async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id);
+        const { password, updatedAt, ...other } = user._doc;
+        res.status(200).json(other);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
 
 module.exports = router;
