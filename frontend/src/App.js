4828714ee2daa99575cc20afe579273c7a59bdf6
aizawa <ikiatawazia@outlook.jp>
2023-01-22 18:05:54 +0900

    Route 画面遷移

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
-// import Home from "./pages/home/Home";
-// import Login from "./pages/login/Login";
+import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
-// import Register from "./pages/register/Register";
-// import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import Profile from "./pages/profile/Profile";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/profile:username" element={<Profile/>}/>
+      </Routes>
+    </Router>
     );
 }
 
