84eaff496c79703cb12edf587bf8d87a6360f754
aizawa <ikiatawazia@outlook.jp>
2023-02-26 00:52:30 +0900

    actionを呼び出すためのdispatch useContextを用いたユーザー状態の取得

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,14 +1,22 @@
-import React, { useRef }from 'react'
+import React, { useContext, useRef }from 'react'
+import { loginCall } from '../../actionCalls';
+import { AuthContext } from '../../state/AuthContext';
 import "./Login.css"
 
 export default function Login() {
   const email = useRef();
   const password = useRef()
-  console.log(email);
+  const {user, isFetching, error, dhispatch} = useContext(AuthContext)
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(password.current.value + email.current.value )
+    // console.log(password.current.value + email.current.value )
+    loginCall({
+      email: email.current.value,
+      password: password.current.value
+    },
+    dispatch
+    )
   }
 
   return (
