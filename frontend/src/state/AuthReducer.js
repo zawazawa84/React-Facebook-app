4df4e9d68a6ab1b7fd2111dfd6f207709b7a7368
aizawa <ikiatawazia@outlook.jp>
2023-02-25 22:16:27 +0900

    Reducerの設定による状態更新の仕組みの構築

---
+++ frontend/src/state/AuthReducer.js
@@ -0,0 +1,23 @@
+const AuthReducer = (state, action) => {
+    switch (action.type) {
+        case "LOGIN_START":
+            return{
+                user: null,
+                isFetching: true,
+                error: false,
+            };
+        case "LOGIN_SUCCESS":
+            return{
+                user: action.payload,
+                isFetching: false,
+                error: false,
+            };
+        case "LOGIN_ERROR":
+            return{
+                user: null,
+                isFetching: false,
+                error: action.payload,
+            };
+        
+    }
+}
